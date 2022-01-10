6aafa0bb8d94c4e711a36e4b97be841dccd7905e
ryukun0808bot <85101267+ryukunbot@users.noreply.github.com>
2022-01-10 20:50:27 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -6,12 +6,6 @@ bot = commands.Bot(command_prefix='/')
 
 
 @bot.event
-async def on_ready():
-    print('{0.user}がログインしました'.format(bot))
-    count = len(bot.guilds)
-    await bot.change_presence(activity=discord.Game(name="Cu!help|" + str(count), type=1))
-
-@bot.event
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
