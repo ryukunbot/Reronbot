6b4cf1bb54f856e3b107c3d2f99e531ac42af7d1
ryukun0808bot <85101267+ryukunbot@users.noreply.github.com>
2022-01-10 20:16:40 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -11,6 +11,10 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
+@bot.event
+async def on_ready():
+    await bot.change_presence(activity=discord.game('My bot status'))
+
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
